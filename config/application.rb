1b65d18ad5f5880b0a84a837e741cd1e21f5493b
MorozovaLiuda <liuda315@gmail.com>
2016-02-10 12:15:23 -0800

    aws

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,13 @@ require 'carrierwave'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+if File.exists?(File.expand_path('../application.yml', __FILE__))
+  ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+end
 
 module SampleApp
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
