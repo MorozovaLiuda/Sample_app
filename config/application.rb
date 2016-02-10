eab4b8776cc216cd96ebab79bc51732ea5417559
MorozovaLiuda <liuda315@gmail.com>
2016-02-10 12:59:08 -0800

    aws

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,6 @@ require 'carrierwave'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-if File.exists?(File.expand_path('../application.yml', __FILE__))
-  ENV.update YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-end
 
 module SampleApp
   class Application < Rails::Application
