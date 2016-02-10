b33237783253d97c85ae64b3aafba3dde5798ca6
MorozovaLiuda <liuda315@gmail.com>
2016-02-10 10:51:13 -0800

    s3 issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,9 @@
         config.fog_credentials = {
           # Configuration for Amazon S3
           :provider               => 'AWS',                        # change var's name
-          :aws_access_key_id      => ENV['S3_ACCESS_KEY'],         # change var's name
-          :aws_secret_access_key  => ENV['S3_SECRETE_KEY']
+          :s3_access_key_id      => ENV['S3_ACCESS_KEY'],         # change var's name
+          :s3_secret_access_key  => ENV['S3_SECRETE_KEY']
+ 
         }
         config.fog_directory     =  ENV['S3_BUCKET']
       end
